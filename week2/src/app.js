f7999f542fe34f783e09d655843a6e88e0ac21e7
Andrew Durham <andrewdurham1094@gmail.com>
2022-03-14 11:14:02 -0600

    week2 assignment

--- week2/src/app.js
+++ week2/src/app.js
@@ -1,20 +1,37 @@
 var object = {
-  name: 'Bob',
-  age: 50
+  name: 'Andrew',
+  favArtist: 'Red Hot Chili Peppers',
+  favTv: 'Breaking Bad',
+  favMovie: 'Jaws'
 };
 
 class Person {
   name;
-  age;
+  favArtist;
+  favTv;
+  favMovie;
 
-  constructor(name, age) {
+  constructor(name, favArtist, favTv, favMovie) {
     this.name = name;
-    this.age = age;
-  }
-
-  getLegal() {
-    return this.age > 18;
+    this.favArtist = favArtist;
+    this.favTv = favTv;
+    this.favMovie = favMovie;
   }
 }
 
-console.log(new Person('Bob', 50));
+function displayFavs() {
+
+  const userName = document.getElementById('name').value
+  const artist = document.getElementById('musicArtist').value
+  const show = document.getElementById('tvShow').value
+  const movie = document.getElementById('movie').value
+  console.log(userName)
+
+  let favsDiv = document.createElement('div');
+  let favsContent = 'Hello ' + userName + ', Your favorite artist is: ' + artist + ', Your favorite TV show is: ' + show + ', and Your favorite Movie is: ' + movie
+  favsDiv.innerHTML = favsContent;
+  favsDiv.setAttribute('id', 'newFavsDiv')
+  document.body.appendChild(favsDiv)
+}
+
+
